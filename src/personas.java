c7ae03e1f2d8d68270513f7b4e87ce88f1e089e5
JhoanM32 <martinezgarciajhoanmanuel@gmail.com>
2024-09-19 07:36:58 -0500

    19 de septiembre

---
+++ src/personas.java
@@ -0,0 +1,11 @@
+public class personas {
+    private String nombre;
+    private int edad;
+    public personas (String nombre,int edad){
+
+        this.nombre= nombre;
+        this.edad = edad;
+
+    }
+    
+}
